28ef0db7df1f9e98b7f78274b22fb2bebe34c1a3
Shivam Mathur <shivam_jpr@hotmail.com>
2020-05-10 13:56:23 +0530

    Filter extensions marked for removal

--- src/cache.ts
+++ src/cache.ts
@@ -10,7 +10,9 @@ export async function run(): Promise<void> {
   try {
     let version: string = await utils.getInput('php-version', true);
     version = version.length > 1 ? version.slice(0, 3) : version + '.0';
-    const extensions: string = await utils.getInput('extensions', true);
+    const extensions = await utils.filterExtensions(
+      await utils.getInput('extensions', true)
+    );
     const key: string = await utils.getInput('key', true);
     const script_path: string = path.join(__dirname, '../src/extensions.sh');
     await exec(
