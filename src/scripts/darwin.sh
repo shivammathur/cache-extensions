5b215c413bb9a97672f4469ab27162c4a548b280
Shivam Mathur <shivam_jpr@hotmail.com>
2021-10-05 12:54:35 +0530

    Add patch for ca-certificate and reset cache on darwin

--- src/scripts/darwin.sh
+++ src/scripts/darwin.sh
@@ -158,6 +158,8 @@ setup_dependencies() {
   if [[ -n "${extensions_array[*]// /}" ]]; then
     add_brew_tap "$php_tap"
     add_brew_tap "$ext_tap"
+    # Remove once updated on the images
+    curl -o "$tap_dir"/"$core_tap"/Formula/ca-certificates.rb -sL https://raw.githubusercontent.com/"$core_tap"/master/Formula/ca-certificates.rb
     for extension in "${extensions_array[@]}"; do
       IFS=' ' read -r -a dependency_array <<<"$(get_dependencies "$extension")"
       IFS=' ' read -r -a extension_array <<<"$(echo "${dependency_array[@]}" | grep -Eo "[a-z]*@" | sed 's/@//' | tr '\n' ' ')"
